232972da16a750f6977a86ba3940b9d1c8defba0
Anaz <anazzubair@yahoo.com>
2016-05-06 22:30:26 +0400

    Finished Login Screen and Daily Report. But Daily Report does not show all data.

---
+++ PumpsReportApplication/LoginWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace PumpsReportApplication
+{
+    /// <summary>
+    /// Interaction logic for LoginWindow.xaml
+    /// </summary>
+    public partial class LoginWindow : Window
+    {
+        public LoginWindow()
+        {
+            InitializeComponent();
+            FocusManager.SetFocusedElement(this, TextBoxUsername);
+        }
+
+        private void ButtonLogin_Click(object sender, RoutedEventArgs e)
+        {
+            if ((TextBoxUsername.Text.Trim().Length == 0) || (TextBoxPassword.Password.Trim().Length == 0))
+                    return;
+
+            using (var db = new PumpsDBEntities())
+            {
+                var user = db.Users.FirstOrDefault(u => u.Username == TextBoxUsername.Text.Trim());
+                if (user == null || user.Passowrd != TextBoxPassword.Password.Trim())
+                {
+                    MessageBox.Show("Invalid Credentials");
+                    return;
+                }
+
+                
+                var mainWindow = new MainWindow();
+                mainWindow.Show();
+                this.Close();
+            }
+        }
+    }
+}
--- PumpsReportApplication/MainWindow.xaml.cs
+++ PumpsReportApplication/MainWindow.xaml.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using Microsoft.Reporting.WinForms;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -23,6 +25,61 @@ namespace PumpsReportApplication
         public MainWindow()
         {
             InitializeComponent();
+            InitializeForm();
+        }
+
+        private void InitializeForm()
+        {
+            FillStationsList();
+            SetDatePickers();
+        }
+
+        private void SetDatePickers()
+        {
+            DatePickerFrom.SelectedDate = DateTime.Now;
+            DatePickerTo.SelectedDate = DateTime.Now;
+        }
+
+        private void FillStationsList()
+        {
+            using(var db = new PumpsDBEntities())
+            {
+                var stations = db.Stations.ToList();
+                ListStations.ItemsSource = stations;
+                ListStations.SelectedIndex = 0;
+            }
+        }
+
+        private void ButtonGenerate_Click(object sender, RoutedEventArgs e)
+        {
+            ReportViewer.Reset();
+
+            using (var db = new PumpsDBEntities())
+            {
+                var dailyReportData = db.DailyReportViews.Where(dr => dr.StationId == (long)ListStations.SelectedValue).ToList();
+                LabelStation.Content = dailyReportData.Count;
+                var pumpReportDataSource = new ReportDataSource("DailyReportDataSet", dailyReportData);
+                ReportViewer.LocalReport.DataSources.Add(pumpReportDataSource);
+                ReportViewer.LocalReport.ReportEmbeddedResource = "PumpsReportApplication.Reports.DailyReport.rdlc";
+                DisableUnwantedExportFormat(ReportViewer, "PDF");
+                DisableUnwantedExportFormat(ReportViewer, "Word");
+                ReportViewer.LocalReport.Refresh();
+                ReportViewer.RefreshReport();
+            }
+        }
+
+        public void DisableUnwantedExportFormat(ReportViewer ReportViewerID, string strFormatName)
+        {
+            FieldInfo info;
+
+            foreach (RenderingExtension extension in ReportViewerID.LocalReport.ListRenderingExtensions())
+            {
+                if (extension.Name.ToUpper().Contains(strFormatName.ToUpper()))
+                {
+                    info = extension.GetType().GetField("m_isVisible", BindingFlags.Instance | BindingFlags.NonPublic);
+                    info.SetValue(extension, false);
+                }
+            }
         }
     }
 }
